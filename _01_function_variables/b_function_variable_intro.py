39206ea2b9c54300990f14f3f6bf99aee85bc435
Osvaldo Ruiz <oruiz.code@gmail.com>
2024-06-25 23:18:39 -0700

    done with 01 package

--- _01_function_variables/b_function_variable_intro.py
+++ _01_function_variables/b_function_variable_intro.py
@@ -8,20 +8,37 @@ def surprise():
     return 'SURPRISE!'
 
 # TODO 1) Change what is assigned into the func_1 variable so test_1 will pass
-func_1 = None
+func_1 = surprise
 
 # TODO 2) Change the return statement below so that test_2 will pass
 def pizza_surprise():
-    return None
+    return "SURPRISE, here's a pizza!"
+
+func_2 = pizza_surprise
 
 # TODO 3) Implement the birthday_surprise function so that test_3 will pass
 def birthday_surprise(years_old):
-    return None
+    birthday_wish = "SURPRISE, Happy "
+    birthday_wish_second_half = " birthday!!!"
+    years_old = str(years_old)
+    if years_old == '1':
+        birthday_wish += years_old +"st" + birthday_wish_second_half 
+    elif years_old == '2':
+        birthday_wish += years_old +"nd" + birthday_wish_second_half 
+    elif years_old == '3':
+        birthday_wish += years_old +"rd" + birthday_wish_second_half 
+    else:
+        birthday_wish += years_old +"th" + birthday_wish_second_half
+    return birthday_wish
 
+func_3 = birthday_surprise
 # TODO 4) Implement the surprise_guests function so that test_3 will pass
 #  *HINT* You will have to add input parameters to the function
-def surprise_guests():
-    return None
+def surprise_guests(message, guests):
+    surprise_guests = ''
+    for guest in guests:
+        surprise_guests += guest + ' says '+ message() +'\n'
+    return surprise_guests
 
 # ================== DO NOT MODIFY THE CODE BELOW ============================
 
